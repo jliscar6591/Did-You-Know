822fd3b42f15ba0a890617a84ecb7c9f0bbed4e4
john liscar <john.liscar@gmail.com>
2017-12-05 14:07:56 -0500

    working omdb pull

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,43 @@
+// fixes titles of videos
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
 
-// These variables will hold the results we get from the user's inputs via HTML
-var showSearch = "";
-var numResults = 0;
+//runs the query though omdb
+function runQuery(queryURL) {
+	
+	$.ajax({
+		url: queryURL, 
+		method: 'GET'
+	}).done(function(response){
+		console.log(response);
+	});
 
-var queryURL = "https://www.omdbapi.com/?t=" + thisShow + "&y=&plot=short&apikey=trilogy";
-
-// Counter to keep track of video numbers as they come in
-var vidCounter = 0;
-
-function runQuery(numArticles, queryURL) {
-		
-		$.ajax({
-			url: queryURL, 
-			method: 'GET'
-		}).done(function(response){
-			var currentVid = response.data;
-
-			console.log(currentVid);
-		});
+}
 
 
-			/*$.each(currentVid, function(index,value){
-				animatedGif= value.images.original.url;
-				pausedGif = value.images.original_still.url;*/
+
+// METHODS
+// ==========================================================
+
+// on.("click") function associated with the Search Button	
+$("#run-search").on("click", function(event) {
+	// This line allows us to take advantage of the HTML "submit" property
+	// This way we can hit enter on the keyboard and it registers the search
+	// (in addition to clicks).
+	event.preventDefault();
+
+	// Grabbing text the user typed into the search input
+	var searchTerm = $("#newMovieInput").val().trim();
+	var searchURL = queryURL ;
+
+	var queryURL = "https://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
+
+	//Number of results the user would like displayed
+	//numResults = $("#num-records-select").val();
+
+	// Then we will pass the final searchURL and the number of results to
+	// include to the runQuery function
+	runQuery(queryURL);
+	youtubeQuery(youtubequeryURL);
+});
+
+$(document).on("click", "#run-search", youtubeQuery);
